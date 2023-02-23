f433bc186add78b697fd87eb6f5ec2edf4075f41
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-23 13:32:07 +0000

    Fixed linter erros

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ import './style.css';
 import TodoList from './modules/TodoList.js';
 
 const tasks = [
-    { description: 'Go to the gym', completed: true, index: 1 },
-    { description: 'Go to canteen for food', completed: true, index: 2 },
-    { description: 'Do laundry', completed: false, index: 3 },
-    { description: 'Cook for family dinner', completed: false, index: 4 },
+  { description: 'Go to the gym', completed: true, index: 1 },
+  { description: 'Go to canteen for food', completed: true, index: 2 },
+  { description: 'Do laundry', completed: false, index: 3 },
+  { description: 'Cook for family dinner', completed: false, index: 4 },
 ];
 
 const todoList = new TodoList(tasks);
