d74cd22e78aaa5ab5dc3879046ee789300689d5a
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-23 13:17:08 +0000

    Fixed linter error

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import './style.css';
-import TodoList from './modules/TodoList.js'
+import TodoList from './modules/TodoList.js';
 
 // class TodoList {
 //   constructor(tasks) {
@@ -22,10 +22,10 @@ import TodoList from './modules/TodoList.js'
 // }
 
 const tasks = [
-    { description: 'Go to the gym', completed: true, index: 1 },
-    { description: 'Go to canteen for food', completed: true, index: 2 },
-    { description: 'Do laundry', completed: false, index: 3 },
-    { description: 'Cook for family dinner', completed: false, index: 4 },
+  { description: 'Go to the gym', completed: true, index: 1 },
+  { description: 'Go to canteen for food', completed: true, index: 2 },
+  { description: 'Do laundry', completed: false, index: 3 },
+  { description: 'Cook for family dinner', completed: false, index: 4 },
 ];
 
 const todoList = new TodoList(tasks);
--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -1,21 +1,21 @@
 class TodoList {
-    constructor(tasks) {
-        this.tasks = tasks;
-    }
+  constructor(tasks) {
+    this.tasks = tasks;
+  }
 
-    render() {
-        const list = document.getElementById('todoList');
-        list.innerHTML = '';
-        this.tasks.sort((a, b) => a.index - b.index);
-        this.tasks.forEach((task) => {
-            const listItem = document.createElement('li');
-            listItem.innerText = task.description;
-            if (task.completed) {
-                listItem.classList.add('completed');
-            }
-            list.appendChild(listItem);
-        });
-    }
+  render() {
+    const list = document.getElementById('todoList');
+    list.innerHTML = '';
+    this.tasks.sort((a, b) => a.index - b.index);
+    this.tasks.forEach((task) => {
+      const listItem = document.createElement('li');
+      listItem.innerText = task.description;
+      if (task.completed) {
+        listItem.classList.add('completed');
+      }
+      list.appendChild(listItem);
+    });
+  }
 }
 
-export default TodoList
+export default TodoList;
