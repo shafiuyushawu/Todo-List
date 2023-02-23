5586ad363e7b6fc394aa693d3fa90a88dfd44dfe
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-02-23 17:32:57 +0000

    Added styling to the page

--- src/index.js
+++ src/index.js
@@ -3,9 +3,8 @@ import TodoList from './modules/TodoList.js';
 
 const tasks = [
   { description: 'Go to the gym', completed: true, index: 1 },
-  { description: 'Go to canteen for food', completed: true, index: 2 },
-  { description: 'Do laundry', completed: false, index: 3 },
-  { description: 'Cook for family dinner', completed: false, index: 4 },
+  { description: 'Go to canteen for food', completed: false, index: 2 },
+
 ];
 
 const todoList = new TodoList(tasks);
--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -8,12 +8,31 @@ class TodoList {
     list.innerHTML = '';
     this.tasks.sort((a, b) => a.index - b.index);
     this.tasks.forEach((task) => {
+      const listDiv = document.createElement('ul');
+      listDiv.classList.add('list__div');
+      const itemDiv = document.createElement('div');
+      itemDiv.classList.add('item__div');
       const listItem = document.createElement('li');
-      listItem.innerText = task.description;
+      listItem.className = 'list__iem';
+      const checkbox = document.createElement('input');
+      checkbox.type = 'checkbox';
+      checkbox.name = 'checkbox';
+
+      const ellipses = document.createElement('span');
+      ellipses.innerHTML = '<i id="edit" class="fa-solid fa-ellipsis-vertical"></i>';
+      const todoText = document.createElement('span');
+      todoText.innerText = task.description;
       if (task.completed) {
-        listItem.classList.add('completed');
+        todoText.classList.add('completed');
       }
-      list.appendChild(listItem);
+
+      list.appendChild(listDiv);
+      listDiv.appendChild(itemDiv);
+      itemDiv.appendChild(checkbox);
+      itemDiv.appendChild(listItem);
+
+      listItem.appendChild(todoText);
+      listDiv.appendChild(ellipses);
     });
   }
 }
