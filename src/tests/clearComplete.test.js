9d11c507784798bfd0b2cf40742d3e2d8463d9c5
Shafiu Ibn Yushawu <yishafiu@gmail.com>
2023-03-03 05:36:20 +0000

    Checked for linter error

--- src/tests/clearComplete.test.js
+++ src/tests/clearComplete.test.js
@@ -1,14 +1,15 @@
-import Todos from '../modules/Todos.js'
+import Todos from '../modules/Todos.js';
+
 describe('clear all completed task', () => {
   it('should remove all completed task  s from  the tasks array', () => {
-    const todos = new Todos()
-    todos.addTask('Eat launch at noon')
-    todos.addTask('Do the dishes')
-    todos.addTask('Hit the gym')
-    todos.allTodos[0].completed = true
-    todos.allTodos[1].completed = true
-    todos.clearCompletedTasks()
-    expect(todos.allTodos.length).toBe(1)
-    expect(todos.allTodos[0].description).toBe('Hit the gym')
-  })
-})
+    const todos = new Todos();
+    todos.addTask('Eat launch at noon');
+    todos.addTask('Do the dishes');
+    todos.addTask('Hit the gym');
+    todos.allTodos[0].completed = true;
+    todos.allTodos[1].completed = true;
+    todos.clearCompletedTasks();
+    expect(todos.allTodos.length).toBe(1);
+    expect(todos.allTodos[0].description).toBe('Hit the gym');
+  });
+});
